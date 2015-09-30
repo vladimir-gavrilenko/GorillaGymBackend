a52d50b92d5161c628386181d94fe1928731ee5d
Egalvi <egalvi@mail.ru>
2015-09-30 22:31:12 +0300

    Client data

--- gorillagym-cart/src/main/java/kg/gorillagym/shop/cart/GorillaGymClientData.java
+++ gorillagym-cart/src/main/java/kg/gorillagym/shop/cart/GorillaGymClientData.java
@@ -3,7 +3,34 @@ package kg.gorillagym.shop.cart;
 import ru.egalvi.shop.ClientData;
 
 /**
+ * Client data for purchase.
  */
 public class GorillaGymClientData implements ClientData {
+    private final String email;
+    private final String name;
+    private final String phone;
+    private final String address;
 
+    public GorillaGymClientData(String email, String name, String phone, String address) {
+        this.email = email;
+        this.name = name;
+        this.phone = phone;
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
 }
