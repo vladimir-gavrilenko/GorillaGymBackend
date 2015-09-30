b87071023c8476b224052672fa52b204d905262a
Egalvi <egalvi@mail.ru>
2015-09-30 23:12:38 +0300

    Mock for prod content-service

---
+++ gorillagym-content-service/src/main/java/kg/gorillagym/shop/content/GorillaGymCategoryService.java
@@ -0,0 +1,14 @@
+package kg.gorillagym.shop.content;
+
+import ru.egalvi.shop.gorillagym.model.Category;
+import ru.egalvi.shop.gorillagym.service.CategoryService;
+
+import java.util.List;
+
+/**
+ */
+public class GorillaGymCategoryService implements CategoryService {
+    public List<Category> getAll() {
+        return null;
+    }
+}
---
+++ gorillagym-content-service/src/main/java/kg/gorillagym/shop/content/GorillaGymProductService.java
@@ -0,0 +1,15 @@
+package kg.gorillagym.shop.content;
+
+import ru.egalvi.shop.gorillagym.model.Category;
+import ru.egalvi.shop.gorillagym.model.Product;
+import ru.egalvi.shop.gorillagym.service.ProductService;
+
+import java.util.List;
+
+/**
+ */
+public class GorillaGymProductService implements ProductService {
+    public List<Product> getForCategory(Category category) {
+        return null;
+    }
+}
