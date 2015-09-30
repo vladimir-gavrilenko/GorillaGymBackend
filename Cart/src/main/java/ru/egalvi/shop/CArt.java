a340d310f4b46d34af454557019cd24d2a0df310
Egalvi <egalvi@mail.ru>
2015-09-30 22:41:01 +0300

    Cart interface

---
+++ Cart/src/main/java/ru/egalvi/shop/CArt.java
@@ -0,0 +1,11 @@
+package ru.egalvi.shop;
+
+import java.io.Serializable;
+
+/**
+ */
+public interface Cart extends Serializable {
+    void add(CartItem item, int quantity);
+
+    void remove(CartItem item, int quantity);
+}
