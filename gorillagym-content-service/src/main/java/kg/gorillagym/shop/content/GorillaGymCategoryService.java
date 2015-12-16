c4b2d045c81689a435a764cffb3823414182b769
Vladimir Gavrilenko <gavrilenko92@gmail.com>
2015-12-16 22:30:32 +0300

    category sorted

--- gorillagym-content-service/src/main/java/kg/gorillagym/shop/content/GorillaGymCategoryService.java
+++ gorillagym-content-service/src/main/java/kg/gorillagym/shop/content/GorillaGymCategoryService.java
@@ -4,26 +4,35 @@ import kg.gorillagym.shop.content.impl.RestClient;
 import kg.gorillagym.shop.content.impl.RestClientFactory;
 import retrofit.Call;
 import ru.egalvi.shop.gorillagym.model.Category;
+import ru.egalvi.shop.gorillagym.model.CategorySortComparator;
 import ru.egalvi.shop.gorillagym.service.CategoryService;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
 
 /**
  */
 public class GorillaGymCategoryService implements CategoryService {
 
-    private final RestClient restClient;
+    private RestClient restClient;
 
     public GorillaGymCategoryService() {
         //TODO move URL to settings
         restClient = RestClientFactory.create("http://100pudov.kg");
     }
 
+    // visible for testing
+    void setRestClient(RestClient restClient) {
+        this.restClient = restClient;
+    }
+
     public List<Category> getAll() {
         Call<List<Category>> categories = restClient.getCategories();
         try {
-            return categories.execute().body();
+            List<Category> result = categories.execute().body();
+            Collections.sort(result, new CategorySortComparator());
+            return result;
         } catch (IOException e) {
             //TODO make own exception and handle it somewhere
             throw new RuntimeException(e);
